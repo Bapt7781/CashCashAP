c7c2376e14b8d1b1cb48d979fcd51f95949c0d37
Baptiste Delisle <baptiste.delisle@gmail.com>
2023-11-29 16:39:39 +0100

    Creation page index.php

--- CashCash/index.php
+++ CashCash/index.php
@@ -1,27 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Accueil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    <!--
-        git clone pour cloner
-        git pull pour recuperer
-        git add fichier qu'on veut ajt
-        git commit -m 'modif' pour commentaire modif
-        git push pour pousser
-    -->
-    <div class="input-group mb-3">
-        <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
-        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-      </div>
 
-      dqzdqzd
-      test
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Matricule d'employé</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Ex : 5">
+</div>
+<label for="inputPassword5" class="form-label">Password</label>
+<input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+    
 </body>
-</html>     
+</html>
