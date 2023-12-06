04359a6f562e7253be75c0c1fc67d6c54f4d74a3
Baptiste Delisle <baptiste.delisle@gmail.com>
2023-12-06 15:02:21 +0100

    ttt

--- CashCash/index.php
+++ CashCash/index.php
@@ -9,13 +9,20 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-
-<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Matricule d'employé</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Ex : 5">
+<div class="container">
+  <div class="row">
+    <div class="col-3 offset-md-4">
+      <div class="mb-5">
+        <label for="exampleFormControlInput1" class="form-label">Matricule d'employé</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Ex : 5">
+      </div>
+      <div class="mb-5">
+        <label for="inputPassword5" class="form-label">Password</label>
+        <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+      </div>
+    </div>
+  </div>
 </div>
-<label for="inputPassword5" class="form-label">Password</label>
-<input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
-    
+
 </body>
 </html>
