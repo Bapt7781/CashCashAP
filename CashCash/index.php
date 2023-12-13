491b31f4d8895a6247dca99293e68c6974c1e615
Baptiste Delisle <baptiste.delisle@gmail.com>
2023-12-13 14:18:27 +0100

    qzdqzd

--- CashCash/index.php
+++ CashCash/index.php
@@ -1,6 +1,5 @@
 <?php 
 include "getRacine.php";
-include "$racine/controleur/controleurPrincipal.php";
 include_once "$racine/modele/authentification.inc.php"; // pour pouvoir utiliser isLoggedOn()
 
 if (isset($_GET["action"])) {
