e120b8053b93f93a5b31c0213f80c04a872a0621
Baptiste Delisle <baptiste.delisle@gmail.com>
2023-12-13 17:11:07 +0100

    qzdqz

--- CashCash/controleur/connexion.php
+++ CashCash/controleur/connexion.php
@@ -21,7 +21,7 @@ else
 login($matriculeU,$mdpU);
 
 if (isLoggedOn()){ // si l'utilisateur est connecté on redirige vers la vue accueil
-    include "$racine/vue/Accueil.php";
+    include "$racine/vue/vueConsulterInterventionAssistant.php";
 echo "7";
 }
 //else{ // l'utilisateur n'est pas connecté, on affiche le formulaire de connexion
