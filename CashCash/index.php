26709e725a1271b298de0990b98bc259d2ff2cc7
Baptiste Delisle <baptiste.delisle@gmail.com>
2023-12-13 13:57:46 +0100

    getRacine

--- CashCash/index.php
+++ CashCash/index.php
@@ -1,4 +1,15 @@
-<?php include_once "$racine/modele/authentification.inc.php"; ?>
+<?php 
+include "getRacine.php";
+include "$racine/controleur/controleurPrincipal.php";
+include_once "$racine/modele/authentification.inc.php"; // pour pouvoir utiliser isLoggedOn()
+
+if (isset($_GET["action"])) {
+    $action = $_GET["action"];
+} 
+else {
+    $action = "defaut";
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
