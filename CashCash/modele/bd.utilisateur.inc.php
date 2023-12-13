cb2a725b5501c1b39b04ab2340f7647d9ca6800d
Baptiste Delisle <baptiste.delisle@gmail.com>
2023-12-13 13:55:02 +0100

    Structure authent

---
+++ CashCash/modele/bd.utilisateur.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once "bd.inc.php";
+
+function getUtilisateurs() {
+
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from employe");
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        while ($ligne) {
+            $resultat[] = $ligne;
+            $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        }
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function getUtilisateurByMatriculeU($matriculeU) {
+
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select * from employe where matricule=:matriculeU");
+        $req->bindValue(':matriculeU', $matriculeUU, PDO::PARAM_STR);
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
