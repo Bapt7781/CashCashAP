a35e9bd440c7f8487f67e0acd1462d5aab044154
Baptiste Delisle <baptiste.delisle@gmail.com>
2023-12-18 17:00:46 +0100

    Accueil

--- CashCash/vue/Accueil.php
+++ CashCash/vue/Accueil.php
@@ -1,10 +1,21 @@
 <?php
 include_once "$racine/modele/authentification.inc.php";
 
+// Vérifiez le rôle de l'utilisateur
+if (isset($_SESSION["role"])) {
+    $role = $_SESSION["role"];
+
+    // Utilisez le rôle pour afficher le contenu approprié
+    if ($role == "technicien") {
+        echo "Bienvenue, technicien!";
+    } elseif ($role == "assistant") {
+        echo "Bienvenue, assistant!";
+    }
+}
 // Traitement du formulaire si le bouton a été soumis
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["logout"])) {
     logout();
-    header("Location: $racine/vue/vueAuthentification.php"); // Redirection vers la page d'authentification
+    header("Location: $racine/controleur/connexion.php"); // Redirection vers la page d'authentification
     exit();
 }
 ?>
@@ -19,6 +30,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["logout"])) {
 <body>
 
 <!-- Contenu de la page d'accueil -->
+
+
+
 
 <!-- Formulaire avec un bouton de déconnexion -->
 <form method="post">
