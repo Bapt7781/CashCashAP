332404c1aaa1aba5d4064439917256a085108e30
martin amblanc <amblancm@gmail.com>
2024-01-11 11:27:09 +0100

    new

--- CashCash/controleur/Fiche.php
+++ CashCash/controleur/Fiche.php
@@ -4,7 +4,11 @@ if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
 }
 include_once "$racine/modele/bd.RechercheFiche.inc.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["numclient"])){
+        $numero_client = $_POST['numero_client'];
 
+        $Recherchefiche = getRecherchefiche($numclient);
+    }
 }
 include "$racine/vue/RechercheFiche.php";
 ?>
--- CashCash/modele/bd.RechercheFiche.inc.php
+++ CashCash/modele/bd.RechercheFiche.inc.php
@@ -1,4 +1,19 @@
 <?php
 include_once "bd.inc.php";
+function getStatistiques($dateDebut, $dateFin){
+    try{
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("");
+    
+    $req->execute();
+    $resultats = $req->fetchAll(PDO::FETCH_ASSOC);
+    
 
+
+        return $resultats;
+    }catch(Exception $e){
+        print "Erreur !:" .$e->getMessage();
+        die();
+    }
+}
 ?>
