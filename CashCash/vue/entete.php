c1364342b21a94bf14a77068c50dd8d19bba7b41
Baptiste Delisle <baptiste.delisle@gmail.com>
2024-01-11 11:24:59 +0100

    entete

--- CashCash/vue/entete.php
+++ CashCash/vue/entete.php
@@ -8,20 +8,9 @@ include_once "$racine/modele/authentification.inc.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-<!-- <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <div class="position-absolute top-0 start-0 mt-3 ms-3">
-    <form method="post" action="./?action=deconnexion">
-        <button type="submit" class="btn btn-secondary" name="logout">Déconnexion</button>
-    </form>
-
-    <h1>CashCash</h1>
-
-    </div>
-  </div>
-</nav> -->
 <div class="container-fluid">
     <nav class="navbar navbar-expand-lg bg-body-tertiary container-fluid">
         <form method="post" action="./?action=deconnexion">
