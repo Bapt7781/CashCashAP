8d68c3e98f49f253f130e4c78516ab6dd09ae5b0
Martin Amblanc <amblancm@gmail.com>
2024-04-10 09:49:40 +0200

    site fini

--- CashCash/vue/OutilsStatistiques.php
+++ CashCash/vue/OutilsStatistiques.php
@@ -30,7 +30,16 @@ if (isset($role) && !empty($role)) {
     <select name="mois">
         <option value="1">Janvier</option>
         <option value="2">Février</option>
-        <!-- Options pour les autres mois... -->
+        <option value="3">Mars</option>
+        <option value="4">Avril</option>
+        <option value="5">Mai</option>
+        <option value="6">Juin</option>
+        <option value="7">Juillet</option>
+        <option value="8">Aout</option>
+        <option value="9">Septembre</option>
+        <option value="10">Octobre</option>
+        <option value="11">Novembre</option>
+        <option value="12">Decembre</option>
     </select>
 
     <label for="annee">Année :</label>
